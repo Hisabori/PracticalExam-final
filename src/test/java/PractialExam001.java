c2d903a10ca3ff8a5ef788fac5ac443996d971d9
creep <grral71@gmail.com>
2022-06-20 14:49:34 +0900

    Initial commit

---
+++ src/test/java/PractialExam001.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class PractialExam001 {
+    public static void main(String[] args){
+
+        LocalDate now=LocalDate.now();
+
+        System.out.println (now);
+        System.out.println("20180786 한정훈");
+    }
+}
